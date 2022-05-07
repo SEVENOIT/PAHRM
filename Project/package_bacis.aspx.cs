2815466ce18fa9750704af3ac32e639f2a5422db
Mishane Perera <mishane1998@gmail.com>
2022-05-07 13:34:57 +0530

    new changes-super admi

---
+++ Project/package_bacis.aspx.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class package_bacis : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void trail_Click(object sender, EventArgs e)
+    {
+        string status = "trail";
+
+    }
+
+    protected void basic_Click(object sender, EventArgs e)
+    {
+        string status = "basic";
+
+    }
+}
